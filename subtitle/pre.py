721f63ebbdeef37512ff10b27cc01d0a5f47353a
halazi <wx5382@gmail.com>
2013-07-28 17:18:27 -0500

    add []

--- subtitle/pre.py
+++ subtitle/pre.py
@@ -14,8 +14,10 @@ def main():
         infilecontent = fin.read()
         all_matches = re.findall(r"\0D\0i\0a\0l\0o\0g\0u\0e\0:[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,[\000-\053\055-\177]*,([\000-\377]*?\000\015\000\012|[\000-\377]*$)", infilecontent)
         for match in all_matches:
-            fout.write(match + "\0\x0D\0\x0A")
-        fout.write("\0\x0D\0\x0A")
+            if match != all_matches[-1]:
+                fout.write(match + "\0[\0]\0\x0D\0\x0A")
+            else:
+                fout.write(match + "\0\x0D\0\x0A\0[\0]\0\x0D\0\x0A")
         fin.close()
         fout.close()
 
