a654d6d69addc3590e80ba1131ebbaed83de8b03
Xun <mhpy@Xuns-MacBook-Pro.local>
2013-09-12 23:33:32 -0700

    three-line format subtitle unix newline

--- subtitle/test.py
+++ subtitle/test.py
@@ -17,20 +17,26 @@ def main():
     split_data = re.split(r"([0-9\-]+:[0-9\-]+:[0-9\-]+:[0-9\-]+ [0-9\-]+:[0-9\-]+:[0-9\-]+:[0-9\-]+)", in_data)
     out_data = split_data[0].strip()
     for i in range(1, len(split_data), 2):
-        out_data += split_data[i]
-        out_data += " "
-        out_data += " ".join(split_data[i+1].split())
-        out_data += "\n"
-    print(out_data)
+        out_data += split_data[i] + "\n"
+        tmp_data = " ".join(split_data[i+1].split())
+        for j in range(len(tmp_data)-1, -1, -1):
+            if ord(tmp_data[j]) >= 0x4E00 and ord(tmp_data[j]) <= 0x9FFF:
+                if j >= len(tmp_data) - 1 or tmp_data.find(" ", j, len(tmp_data)) == -1:
+                    out_data += tmp_data + "\n\n"
+                    break
+                else:
+                    out_data += tmp_data[0:j+1] + "\n" + tmp_data[j+1:].strip() + "\n"
+                    break
 
+    #print(out_data)
     if len(sys.argv) == 2:
         out_file_name = in_file_name + ".txt"
     else:
         out_file_name = sys.argv[2]
     out_file_encoding = in_file_encoding
-    #fout = open(out_file_name, "wt", encoding=out_file_encoding)
-    #fout.write(out_data)
-    #fout.close()
+    fout = open(out_file_name, "wt", encoding=out_file_encoding)
+    fout.write(out_data)
+    fout.close()
 
 if __name__ == '__main__':
     main()
